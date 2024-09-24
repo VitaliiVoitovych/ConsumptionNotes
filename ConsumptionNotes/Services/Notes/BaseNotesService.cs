bdbd22a970f753bc760859510d5e431de532a843
Vitalii Voitovych <vitalikvv2119@gmail.com>
2024-09-24 21:59:08 +0300

    Refactoring in notes services

--- ConsumptionNotes/Services/Notes/BaseNotesService.cs
+++ ConsumptionNotes/Services/Notes/BaseNotesService.cs
@@ -1,22 +1,53 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Linq;
 
 namespace ConsumptionNotes.Services.Notes;
 
-public abstract class BaseNotesService<TNote, TChartService> : ObservableObject
+public abstract partial class BaseNotesService<TNote, TChartService> : ObservableObject
     where TNote : BaseConsumption
     where TChartService : BaseChartService<TNote>
 {
+    [ObservableProperty] private decimal _averageAmount;
+    
     public TChartService ChartService { get; }
 
+    public ObservableCollection<TNote> Consumptions { get; }
+    
     protected BaseNotesService(TChartService chartService)
     {
         ChartService = chartService;
+        Consumptions = [];
+    }
+
+    public void AddNote(TNote note)
+    {
+        if (Consumptions.Any(n => EqualsYearAndMonth(n.Date, note.Date)))
+            throw new ArgumentException("Запис про цей місяць вже є");
+
+        var lastConditionNote = Consumptions.LastOrDefault(n => n.Date < note.Date);
+        var index = Consumptions.IndexOf(lastConditionNote!) + 1;
+        
+        Consumptions.Insert(index, note);
+        ChartService.AddValues(index, note);
+        
+        UpdateAverageValues();
+    }
+
+    public void RemoveNote(TNote note)
+    {
+        var index = Consumptions.IndexOf(note);
+        Consumptions.RemoveAt(index);
+        ChartService.RemoveValues(index);
+        
+        UpdateAverageValues();
+    }
+
+    protected virtual void UpdateAverageValues()
+    {
+        AverageAmount = Consumptions.Count > 0 
+            ? Consumptions.Average(e => e.AmountToPay) 
+            : 0.0m;
     }
     
-    public abstract void AddNote(TNote note);
-    public abstract void RemoveNote(TNote note);
-    protected abstract void UpdateAverageValues();
-    
-    protected bool EqualsYearAndMonth(DateOnly date1, DateOnly date2)
+    private static bool EqualsYearAndMonth(DateOnly date1, DateOnly date2)
         => (date1.Month, date1.Year) == (date2.Month, date2.Year);
 }
--- ConsumptionNotes/Services/Notes/NaturalGasNotesService.cs
+++ ConsumptionNotes/Services/Notes/NaturalGasNotesService.cs
@@ -1,55 +1,21 @@
 ﻿using System.Linq;
-using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ConsumptionNotes.Services.Notes;
 
 public partial class NaturalGasNotesService : BaseNotesService<NaturalGasConsumption, NaturalGasChartService>
 {
-    [ObservableProperty] private decimal _averageAmount;
     [ObservableProperty] private double _averageCubicMeterConsumed;
-    
-    public ObservableCollection<NaturalGasConsumption> NaturalGasConsumptions { get; }
     
     public NaturalGasNotesService(NaturalGasChartService chartService) : base(chartService)
     {
-        NaturalGasConsumptions = [];
-    }
-
-    public override void AddNote(NaturalGasConsumption note)
-    {
-        if (NaturalGasConsumptions.Any(n => EqualsYearAndMonth(n.Date, note.Date)))
-            throw new ArgumentException("Запис про цей місяць вже є");
-
-        var lastConditionNote = NaturalGasConsumptions.LastOrDefault(n => n.Date < note.Date);
-        var index = NaturalGasConsumptions.IndexOf(lastConditionNote!) + 1;
-        
-        NaturalGasConsumptions.Insert(index, note);
-        ChartService.AddValues(index, note);
-        
-        UpdateAverageValues();
-    }
-
-    public override void RemoveNote(NaturalGasConsumption note)
-    {
-        var index = NaturalGasConsumptions.IndexOf(note);
-
-        NaturalGasConsumptions.RemoveAt(index);
-        ChartService.RemoveValues(index);
-        
-        UpdateAverageValues();
     }
 
     protected override void UpdateAverageValues()
     {
-        if (NaturalGasConsumptions.Count > 0)
-        {
-            AverageAmount = NaturalGasConsumptions.Average(e => e.AmountToPay);
-            AverageCubicMeterConsumed = NaturalGasConsumptions.Average(e => e.CubicMeterConsumed);
-        }
-        else
-        {
-            AverageAmount = 0.0m;
-            AverageCubicMeterConsumed = 0.0;
-        }
+        base.UpdateAverageValues();
+
+        AverageCubicMeterConsumed = Consumptions.Count > 0 
+            ? Consumptions.Average(e => e.CubicMeterConsumed) 
+            : 0.0;
     }
 }
