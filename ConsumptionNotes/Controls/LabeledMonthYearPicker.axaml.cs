a068b9d2fd20f1eb880a4a286d25e22959d26321
Vitalii Voitovych <vitalikvv2119@gmail.com>
2024-09-28 23:41:28 +0300

    Added custom controls for date and numeric selection

---
+++ ConsumptionNotes/Controls/LabeledMonthYearPicker.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+
+namespace ConsumptionNotes.Controls;
+
+public partial class LabeledMonthYearPicker : UserControl
+{
+    public static readonly StyledProperty<string?> TextProperty = AvaloniaProperty.Register<LabeledMonthYearPicker, string?>(
+        nameof(Text));
+
+    public string? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public static readonly StyledProperty<DateTimeOffset?> SelectedDateProperty = AvaloniaProperty.Register<LabeledMonthYearPicker, DateTimeOffset?>(
+        nameof(SelectedDate));
+
+    public DateTimeOffset? SelectedDate
+    {
+        get => GetValue(SelectedDateProperty);
+        set => SetValue(SelectedDateProperty, value);
+    }
+    
+    public LabeledMonthYearPicker()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ConsumptionNotes/Controls/LabeledNumericUpDown.axaml.cs
@@ -0,0 +1,56 @@
+﻿using Avalonia;
+
+namespace ConsumptionNotes.Controls;
+
+public partial class LabeledNumericUpDown : UserControl
+{
+    public static readonly StyledProperty<string?> TextProperty = AvaloniaProperty.Register<LabeledNumericUpDown, string?>(
+        nameof(Text));
+
+    public string? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public static readonly StyledProperty<decimal> MinimumProperty = AvaloniaProperty.Register<LabeledNumericUpDown, decimal>(
+        nameof(Minimum));
+
+    public decimal Minimum
+    {
+        get => GetValue(MinimumProperty);
+        set => SetValue(MinimumProperty, value);
+    }
+
+    public static readonly StyledProperty<decimal> MaximumProperty = AvaloniaProperty.Register<LabeledNumericUpDown, decimal>(
+        nameof(Maximum));
+
+    public decimal Maximum
+    {
+        get => GetValue(MaximumProperty);
+        set => SetValue(MaximumProperty, value);
+    }
+
+    public static readonly StyledProperty<decimal> IncrementProperty = AvaloniaProperty.Register<LabeledNumericUpDown, decimal>(
+        nameof(Increment));
+
+    public decimal Increment
+    {
+        get => GetValue(IncrementProperty);
+        set => SetValue(IncrementProperty, value);
+    }
+
+    public static readonly StyledProperty<decimal?> ValueProperty = AvaloniaProperty.Register<LabeledNumericUpDown, decimal?>(
+        nameof(Value));
+
+    public decimal? Value
+    {
+        get => GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+    
+    public LabeledNumericUpDown()
+    {
+        InitializeComponent();
+    }
+}
