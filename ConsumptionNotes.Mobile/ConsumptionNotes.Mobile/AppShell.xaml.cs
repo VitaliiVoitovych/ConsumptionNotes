c75d272001a656df5901ed5a2243578d8a8bfd43
Vitalii Voitovych <vitalii.voitovych@protonmail.com>
2024-12-19 20:32:25 +0200

    Add Mobile client project files

---
+++ ConsumptionNotes.Mobile/ConsumptionNotes.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsumptionNotes.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
